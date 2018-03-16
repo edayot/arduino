87dee031f78d79e44cdf6dfd1db603a7c8a551ca
edayot <pro.e.dayot@netcourrier.com>
2018-03-16 16:44:10 +0100

    Tp1 fini

--- .build/mzero_pro_bl_dbg/preproc/ctags_target_for_gcc_minus_e.cpp
+++ .build/mzero_pro_bl_dbg/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,12 +1,14 @@
+# 1 "c:\\Users\\eleve\\Desktop\\erwan\\erwan-arduino\\main.ino"
+# 1 "c:\\Users\\eleve\\Desktop\\erwan\\erwan-arduino\\main.ino"
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include "Arduino.h"
+# 5 "c:\\Users\\eleve\\Desktop\\erwan\\erwan-arduino\\main.ino" 2
 
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-    pinMode(13, OUTPUT);
+    pinMode(13, (0x1));
 }
 
 // the loop function runs over and over again forever
@@ -17,8 +19,8 @@ void loop()
     potPosition = analogPead(A0);
 
 
-    digitalWrite(13, HIGH);
+    digitalWrite(13, (0x1));
     delay(potPosition);
-    digitalWrite(13, LOW);
+    digitalWrite(13, (0x0));
     delay(potPosition);
 }
--- .build/mzero_pro_bl_dbg/sketch/main.ino.cpp
+++ .build/mzero_pro_bl_dbg/sketch/main.ino.cpp
@@ -1,9 +1,16 @@
+#line 1 "c:\\Users\\eleve\\Desktop\\erwan\\erwan-arduino\\main.ino"
+#line 1 "c:\\Users\\eleve\\Desktop\\erwan\\erwan-arduino\\main.ino"
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
 #include "Arduino.h"
 
 // the setup function runs once when you press reset or power the board
+#line 7 "c:\\Users\\eleve\\Desktop\\erwan\\erwan-arduino\\main.ino"
+void setup();
+#line 13 "c:\\Users\\eleve\\Desktop\\erwan\\erwan-arduino\\main.ino"
+void loop();
+#line 7 "c:\\Users\\eleve\\Desktop\\erwan\\erwan-arduino\\main.ino"
 void setup()
 {
     pinMode(13, OUTPUT);
@@ -22,3 +29,4 @@ void loop()
     digitalWrite(13, LOW);
     delay(potPosition);
 }
+
