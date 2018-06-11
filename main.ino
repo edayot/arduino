674eacc4f045274c9924576fe7cc3c951586ec57
ThaibaultMonfroy <thibault.monfroy.pro@gmail.com>
2018-06-11 10:07:19 +0200

    modifs

--- main.ino
+++ main.ino
@@ -6,6 +6,8 @@
 #include "include/Grove_I2C_Motor_Driver.h"
 #include "include/Ultrasonic.h"
 
+#define I2C_ADDRESS 0x0A
+
 /// The class fsm_state is a type whose value is restricted to a
 /// subset of values
 enum class fsm_state
@@ -17,6 +19,8 @@ enum class fsm_state
 // Declaration of a variable of type fsm_state
 fsm_state my_state = fsm_state::s0;
 
+Ultrasonic ultrasonic(2);
+
 void automate()
 {
     static int ms = 0;
@@ -26,18 +30,35 @@ void automate()
     {
         ms = 0;
 
+        static long RangeInCentimeters;
+
+        RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+        delay(250);
+
         // Manage the state transitions from the state value
         switch (my_state)
         {
             case fsm_state::s0:
 
-                my_state = fsm_state::s1;
+                Motor.speed(MOTOR1, 50);
+                Motor.speed(MOTOR2, -70);
+
+                if(RangeInCentimeters < 5)
+                {
+                    my_state = fsm_state::s1;
+                }
 
                 break;
 
             case fsm_state::s1:
 
-                my_state = fsm_state::s0;
+                Motor.stop(MOTOR1);
+                Motor.stop(MOTOR2);
+
+                if (RangeInCentimeters >= 5)
+                {
+                    my_state = fsm_state::s0;
+                }
 
                 break;
         }
@@ -50,28 +71,14 @@ void automate()
 
 void setup()
 {
-    pinMode(13, OUTPUT);
 
     // Make the function automate() being called
     // every 1000 microseconds
     set_timer4_interrupt(1000, automate);
+    Motor.begin(I2C_ADDRESS);
 }
 
 void loop()
 {
-    // Manage the outputs from the state value
-    switch (my_state)
-    {
-        case fsm_state::s0:
 
-            digitalWrite(13, LOW);
-
-            break;
-
-        case fsm_state::s1:
-
-            digitalWrite(13, HIGH);
-
-            break;
-    }
 }
